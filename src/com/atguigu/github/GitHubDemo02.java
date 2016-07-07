2680f38af29063004cbeb28e95a35f05220b601e
zzyybs <zzyybs@126.com>
2016-07-07 18:18:32 +0800

    Update GitHubDemo02.java

--- src/com/atguigu/github/GitHubDemo02.java
+++ src/com/atguigu/github/GitHubDemo02.java
@@ -4,10 +4,6 @@ public class GitHubDemo02
 {
 	public static void main(String[] args)
 	{
-		System.out.println("0607 在硅谷学些github");
-		System.out.println("github accept,收到");
-		System.out.println("github update 01");
-		System.out.println("local update 01");
-		System.out.println("local update 02");
+		System.out.println("在硅谷学些github");
 	}
 }
