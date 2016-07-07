98b565f9eccb1d26e6bedc30ce2f5c96c81b1c46
zzyybs <zzyybs@126.com>
2016-07-07 18:18:11 +0800

    Update Client.java

--- src/com/atguigu/github/Client.java
+++ src/com/atguigu/github/Client.java
@@ -5,7 +5,5 @@ public class Client
 	public static void main(String[] args)
 	{
 		System.out.println("hello github......");
-		System.out.println("666666666666666666");
-		System.out.println("hello github 2016");
 	}
 }
